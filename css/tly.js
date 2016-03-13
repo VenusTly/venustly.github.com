a110a7d4bb97da2edaa0c9ebc8b05fb2d5aa63c5
venusTLY <2039756737@qq.com>
2016-03-13 19:20:00 +0800

    test

---
+++ css/tly.js
@@ -0,0 +1,25 @@
+
+function clia() {
+	var div1=document.getElementById('div1');
+	var div2=document.getElementById('div2');
+	var div3=document.getElementById('div3');
+	div1.style.display='block';
+	div2.style.display='none';
+	div3.style.display='none';
+}
+function clib() {
+	var div1=document.getElementById('div1');
+	var div2=document.getElementById('div2');
+	var div3=document.getElementById('div3');
+	div1.style.display='none';
+	div2.style.display='block';
+    div3.style.display='none';
+}
+function clia() {
+	var div1=document.getElementById('div1');
+	var div2=document.getElementById('div2');
+	var div3=document.getElementById('div3');
+	div1.style.display='none';
+	div2.style.display='none';
+    div3.style.display='block';
+}
